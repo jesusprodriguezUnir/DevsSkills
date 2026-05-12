9dc825928ecee6f6c586befeb45531434ccd230d
Jesús Pedro Rodriguez <jesuspedro.rodriguez@unir.net>
2026-05-12 06:41:19 +0200

    feat: agregar configuración condicional para el despliegue en Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+const isVercel = process.env.VERCEL === '1';
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://jesusprodriguezUnir.github.io',
-  base: '/DevsSkills',
+  site: isVercel
+    ? 'https://devs-skills.vercel.app'
+    : 'https://jesusprodriguezUnir.github.io',
+  base: isVercel ? '/' : '/DevsSkills',
 });
